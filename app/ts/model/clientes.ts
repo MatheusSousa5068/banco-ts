3d582d580200079ec507a7e2248ab629a0b71236
MatheusSousa5068 <matheus5068trab@gmail.com>
2023-09-14 09:02:40 -0300

    adicionando clientes

---
+++ app/ts/model/clientes.ts
@@ -0,0 +1,28 @@
+class Clientes {
+    private clientes: Array<Cliente>
+    constructor() {
+        this.clientes = new Array<Cliente>();
+    }
+
+    inserir(cliente: Cliente): void {
+        this.clientes.push(cliente)
+    }
+
+    remover(cpf: string): void {
+        const clienteARemover: Cliente = this.pesquisar(cpf);
+        if (clienteARemover) {
+            const indiceCliente = this.clientes.indexOf(clienteARemover);
+            if (indiceCliente > -1) {
+                this.clientes.splice(indiceCliente, 1);
+            }
+        }
+    }
+
+    listar(): Array<Cliente> {
+        return this.clientes
+    }
+
+    pesquisar(cpf: string): Cliente {
+        return this.clientes.find(cliente => cliente.cpf === cpf)
+    }
+}
